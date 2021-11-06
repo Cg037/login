91f5da675ecc7e67818c4b44ec6ecea488b24247
Cg037 <89976491+Cg037@users.noreply.github.com>
2021-11-06 13:21:39 +0100

    Update Login.js

--- Login.js
+++ Login.js
@@ -1,13 +1,16 @@
 
 const namn = "test"; 
 const losenord = "1234";
+let loginForm = document.getElementById("form"); 
 
-let loginForm = document.getElementById("form"); // Kolla vid start om det redan finns ett värde sparat i storage.
-
+// Kolla vid start om det redan finns ett värde sparat i storage.
 if (localStorage.getItem("User") !== null) { 
-    document.body.innerHTML = "<h2>Du är redan inloggad!</h2>"; 
+    document.body.innerHTML = "<h2>Du är redan inloggad!</h2>";
+    
     let btn = document.createElement("BUTTON"); 
-    btn.innerHTML = "LOGGA UT"; document.body.appendChild(btn); // En knapp som loggar ut användaren och tömmer värden i storage och återskapar html-koden för formuläret.
+    btn.innerHTML = "LOGGA UT"; document.body.appendChild(btn);
+    
+    // En knapp som loggar ut användaren och tömmer värden i storage och återskapar html-koden för formuläret.
     btn.addEventListener("click", function logOut(){ 
         localStorage.clear(); document.body.innerHTML = "";
         document.body.appendChild(loginForm);
@@ -15,15 +18,20 @@ if (localStorage.getItem("User") !== null) {
 } 
         
         
-function logIn() { // Tar in värde från formuläret och tilldelar det till 2 variabler.
+function logIn() {
+    // Tar in värde från formuläret och tilldelar det till 2 variabler.
     let user = document.getElementById("userName").value;
-    let pass = document.getElementById("password").value; // Jämför ifall de inmatade värderna stämmer med mina redan skapade inlogsvärden.
+    let pass = document.getElementById("password").value;
+    
+    // Jämför ifall de inmatade värderna stämmer med mina redan skapade inlogsvärden.
         if (user == namn && pass == losenord){ 
             localStorage.setItem("User", user) 
-            document.body.innerHTML = "<h2>Välkommen!</h2>"; 
+            document.body.innerHTML = "<h2>Välkommen!</h2>";
+            
             let btn = document.createElement("BUTTON");
             btn.innerHTML = "LOGGA UT";
             document.body.appendChild(btn);
+            
             btn.addEventListener("click",
             function logOut(){
                 localStorage.clear();document.body.innerHTML = "";
@@ -32,10 +40,13 @@ function logIn() { // Tar in värde från formuläret och tilldelar det till 2 v
         } 
     
     else {
-        document.body.innerHTML = "<h2>Fel användarnamn eller lösenord!</h2>"; // En knapp som vid felaktig inmatning återskapar första sedan med inloggningsformuläret.
+        document.body.innerHTML = "<h2>Fel användarnamn eller lösenord!</h2>";
+        
+        // En knapp som vid felaktig inmatning återskapar första sedan med inloggningsformuläret.
         let btnRetry = document.createElement("BUTTON");
         btnRetry.innerHTML = "FÖRSÖK IGEN"; 
         document.body.appendChild(btnRetry);
+        
         btnRetry.addEventListener("click",
         function retry(){ 
             document.body.innerHTML = ""; 
